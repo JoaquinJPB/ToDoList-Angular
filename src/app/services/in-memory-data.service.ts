62786c5845fa4ee4f7da5ab50629dbb0f20445e3
JoaquinJPB <jjpb2000@gmail.com>
2022-04-21 17:11:09 +0100

    Improving Css style

--- src/app/services/in-memory-data.service.ts
+++ src/app/services/in-memory-data.service.ts
@@ -9,12 +9,12 @@ export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const todos = 
     [
-      { id: 1, title: 'Práctica: Enrutamiento y Navegación', description: "Crea una SPA en Angular que gestione una TO-DO list (CRUD).", completed: false },
-      { id: 2, title: 'Glosario TSDSI', description: "Glosario del tema: Sistema de información", completed: true },
-      { id: 3, title: 'TFT-01 & TFT-02', description: "Entrega de los documentos referentes al TFG", completed: true },
+      { id: 1, title: 'Práctica: Enrutamiento y Navegación', description: "SPA en Angular que gestione una To-Do list (CRUD).", completed: false },
+      { id: 2, title: 'Glosario TSDSI', description: "Glosario del tema: Sistema de información y sus respectivas referencias", completed: true },
+      { id: 3, title: 'TFT-01 & TFT-02', description: "Entrega de los documentos referentes al TFG antes del 25 de abril", completed: true },
       { id: 4, title: 'Práctica 4: Correo', description: "Práctica de correo correspondiente a la práctica 4 de SSR", completed: false },
-      { id: 5, title: 'Práctica: Ingeniería inversa', description: "Crea la documentación de la aplicación que encontrarás en la carpeta Ingeniería Inversa y utiliza dicha aplicación para desarrollar una nueva aplicación que gestione la plantilla de jugadores de un equipo deportivo.", completed: false },
-      { id: 5, title: 'Entrega Sprint 1', description: "Entrega del Sprint 1 de la asignatura MDA", completed: false }
+      { id: 5, title: 'Práctica: Ingeniería inversa', description: "Crea la documentación de la aplicación correspondiente", completed: false },
+      { id: 5, title: 'Entrega Sprint 1', description: "Entrega del Sprint 1 de la asignatura MDA y su correspondiente documentación", completed: false }
     ];
     return {todos};
   }
